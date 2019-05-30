48bf4dd7bdfa3273a603cbf23146317ae52fb6b5
j2jdev <42616389+j2jdev@users.noreply.github.com>
2019-05-30 13:16:32 +0700

    Update auth_users.sql

--- auth_users.sql
+++ auth_users.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : template
-
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
-
-Date: 2019-05-30 13:15:01
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
